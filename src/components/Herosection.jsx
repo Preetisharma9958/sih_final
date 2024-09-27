df4fe568edd865941766506f77dcb25cffe1ab71
priyaverma0604 <144715182+priyaverma0604@users.noreply.github.com>
2024-09-27 20:39:07 +0530

    herosection updated

--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -10,30 +10,43 @@ const HeroSection = () => {
           src="https://upload.wikimedia.org/wikipedia/en/thumb/5/57/Narcotics_Control_Bureau_Seal.svg/140px-Narcotics_Control_Bureau_Seal.svg.png"
           alt="NCB Logo"
           className="ncb-logo"
-        />
+        />{" "}
+        <div className="ncb-text">
+          <p>NCB</p>
+        </div>
         <h1 className="hero-title">
-          Tracking Transactions, Securing the Future.
+          Tracking Transactions, <br />
+          Securing the Future.
         </h1>
-        <p>
+        <p className="text-content">
           A blockchain model for tracing illegal cryptocurrency transactions,
-          detecting suspicious wallets, and strengthening financial security.
+          detecting <br /> suspicious wallets, and strengthening financial
+          security.
         </p>
-        <button className="login-button">Login</button>
-
+        <button className="login-button">LOGIN</button>
         {/* Cryptocurrency Icons */}
         <div className="crypto-icons">
           <img
+            id="Bitcoin"
             src="./icons/bitcoin.png"
             alt="Bitcoin"
             className="crypto-icon"
           />
           <img
+            id="Ethereum"
             src="./icons/ethereum.png"
             alt="Ethereum"
             className="crypto-icon"
           />
-          <img src="./icons/doge.png" alt="Dogecoin" className="crypto-icon" />
           <img
+            id="Dogecoin"
+            src="./icons/doge.png"
+            alt="Dogecoin"
+            className="crypto-icon"
+          />
+
+          <img
+            id="polygon"
             src="./icons/polygon.png"
             alt="Polygon"
             className="crypto-icon"
